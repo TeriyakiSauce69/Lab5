93bc1e0353c385b5c24ab5ffbf5e2475b89fdf1a
root <guccifuji73@gmail.com>
2023-04-03 21:18:28 +0000

    not sure

--- scripts/shpere_fit.py
+++ scripts/shpere_fit.py
@@ -10,6 +10,7 @@ sphere_param_msg = SphereParams()
 xyz_array = np.array([])
 start = False
 
+
 def xyz_coord(data):
 #Make it global so we can access outside
 	global xyz_array
@@ -24,9 +25,13 @@ def xyz_coord(data):
 def sphere_param(data):
 	global sphere_param_msg 
 	sphere_param_msg.xc = data[0]
+	
 	sphere_param_msg.yc = data[1]
+	
 	sphere_param_msg.zc = data[2]
+	
 	sphere_param_msg.radius = math.sqrt(data[0] ** 2 + data[1] ** 2 + data[2] ** 2 + data[3])
+
 	
 	
 def do_math(data):
@@ -45,38 +50,41 @@ def do_math(data):
 	
 	return None
 	
-def noise_filter(P, Prev = None):
-	if Prev == None:
+def noise_filter(P, Prev,first_time):
+	if first_time:
+		#Got these from printing unfiltered data from first iteration
+		fil_gain = 0.03
+		r_gain = 0.026
+		P.xc = fil_gain * P.xc + (1 - fil_gain) * -0.014
+		P.yc = fil_gain * P.yc + (1 - fil_gain) * -0.017	
+		P.zc = fil_gain * P.zc + (1 - fil_gain) * 0.48
+		P.radius = r_gain * P.radius + (1 - r_gain) * 0.055
 		return P
+		
+	fil_gain = 0.03
+	r_gain = 0.026
 	
-	fil_gain = 1
-	r_gain = 0.02
-	
-	#Values derived through trial and error
 	P.xc = fil_gain * P.xc + (1 - fil_gain) * Prev.xc
 	P.yc = fil_gain * P.yc + (1 - fil_gain) * Prev.yc	
 	P.zc = fil_gain * P.zc + (1 - fil_gain) * Prev.zc
-	
-	
 	P.radius = r_gain * P.radius + (1 - r_gain) * Prev.radius
 		
 	return P
 
 if __name__ == '__main__':
+	
 	# initialize the node
 	rospy.init_node('sphere_fit', anonymous = True)
 
-	
 	#Subscribe to get the XYZ stuff
 	rospy.Subscriber('/xyz_cropped_ball', XYZarray, xyz_coord)
 	
 	#Publish final results
 	pos_pub = rospy.Publisher('/sphere_params', SphereParams, queue_size = 1)
 	
-	
 	# set a 10Hz frequency for this loop
 	loop_rate = rospy.Rate(10)
-	
+
 	
 	first_pass = True
 	
@@ -85,26 +93,17 @@ if __name__ == '__main__':
 		if start:	
 			#Call function with data gotten from xyz_coord
 			P = do_math(xyz_array)
-			
 		
-			if first_pass != True:
-				filt_sphere_param_msg = noise_filter(sphere_param_msg, filt_sphere_param_msg)
+			if first_pass == False:
+				filt_sphere_param_msg = noise_filter(sphere_param_msg, filt_sphere_param_msg, False)
+				#Publish data
 				pos_pub.publish(filt_sphere_param_msg)
 				
-			if first_pass:
-				#Filter Noise
-				filt_sphere_param_msg = noise_filter(sphere_param_msg)
-				
+			
+			if first_pass: 
+				filt_sphere_param_msg = noise_filter(sphere_param_msg,sphere_param_msg, True )
 				first_pass = False
-				print("This should only be hit once!")
 				
-		
-			#Publish data
-			#pos_pub.publish(sphere_param_msg)
-			
-			#Publish data
-			
-			
 		loop_rate.sleep()
 		
 			
